f958f5bcd0b1555f06857dee200f805e8defe340
dipankar-halder <dipankar15-11137@diu.edu.bd>
2024-06-13 10:52:51 +0600

    control opacity

--- medi-client/src/Components/Pages/Home/Consultants/Consultants.js
+++ medi-client/src/Components/Pages/Home/Consultants/Consultants.js
@@ -18,50 +18,65 @@ const Consultants = () => {
     <div
       style={{
         backgroundImage:
-          "url('https://www.asterhospitals.in/sites/default/files/2022-03/best-urology-hospital-in-hyderabad.jpg')",
+          "url('https://static.vecteezy.com/system/resources/previews/006/712/977/original/abstract-health-medical-science-healthcare-icon-digital-technology-doctor-concept-modern-innovation-treatment-medicine-on-hi-tech-future-blue-background-for-wallpaper-template-web-design-vector.jpg')",
         backgroundSize: 'cover',
         backgroundAttachment: 'fixed',
+        position: 'relative',
       }}
-      className=" px-20 py-12"
+      className=" px-20 py-12 "
     >
-      <div className="flex items-center text-orange-600 text-xl font-semibold">
-        <h1 className="mr-2">Doctors</h1>
-        <FaMinus />
-        <FaUtensilSpoon className="rotate-45" />
-      </div>
-      <div>
-        <h1 className="font-semibold text-3xl mb-10 text-white">
-          Our Consultants
-        </h1>
-      </div>
-      <div className="grid grid-cols-4 gap-12 mt-5">
-        {doctors
-          .slice(0, 4)
-          .reverse()
-          .map(doctor => (
-            <div className="flex justify-center">
-              <div className="text-center">
-                <img
-                  className="h-56 w-56 rounded-full bg-slate-200"
-                  src={doctor?.img}
-                  alt={doctor?.name}
-                />
-                <h1 className="text-2xl font-semibold mt-2 text-orange-600">
-                  {doctor?.name}
-                </h1>
-                <p className="text-sm uppercase text-slate-50">
-                  {' '}
-                  {doctor?.department}
-                </p>
-                <button
-                  onClick={() => handleDetails(doctor._id)}
-                  className="btn btn-sm btn-warning mt-2"
-                >
-                  View Details
-                </button>
-              </div>
-            </div>
-          ))}
+      <div
+        style={{
+          // position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: 'rgba(61, 61, 61, 0.3)',
+        }}
+        className="p-4 rounded-xl"
+      >
+        <div style={{ position: 'relative', zIndex: 1 }}>
+          <div className="flex items-center text-orange-600 text-xl font-semibold">
+            <h1 className="mr-2">Doctors</h1>
+            <FaMinus />
+            <FaUtensilSpoon className="rotate-45" />
+          </div>
+          <div>
+            <h1 className="font-semibold text-3xl mb-10 text-white">
+              Our Consultants
+            </h1>
+          </div>
+          <div className="grid grid-cols-4 gap-12 mt-5">
+            {doctors
+              .slice(0, 4)
+              .reverse()
+              .map(doctor => (
+                <div className="flex justify-center border-b-2 border-secondary pb-2">
+                  <div className="text-center">
+                    <img
+                      className="h-56 w-56 rounded-full bg-slate-200"
+                      src={doctor?.img}
+                      alt={doctor?.name}
+                    />
+                    <h1 className="text-2xl font-semibold mt-2 text-orange-600">
+                      {doctor?.name}
+                    </h1>
+                    <p className="text-sm uppercase text-slate-50">
+                      {' '}
+                      {doctor?.department}
+                    </p>
+                    <button
+                      onClick={() => handleDetails(doctor._id)}
+                      className="btn btn-sm btn-warning mt-2"
+                    >
+                      View Details
+                    </button>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
       </div>
     </div>
   );
